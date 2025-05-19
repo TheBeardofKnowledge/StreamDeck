6ffc97b7df5bca1b8453a82a52444e885223f46f
The Beard of Knowledge <thebeardofknowledge956@gmail.com>
2025-05-19 16:54:06 -0500

    Update StreamDeckPortFifine.bat
    fixed formatting and removed error handling added during testing/creation.

--- StreamDeckPortFifine.bat
+++ StreamDeckPortFifine.bat
@@ -6,23 +6,22 @@ ECHO Running Admin shell
 ECHO =============================
  
 :checkPrivileges 
-NET FILE 1>NUL 2>NUL
-if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges ) 
+	NET FILE 1>NUL 2>NUL
+	if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges ) 
 :getPrivileges
 :Not elevated, so re-run with elevation
-powershell -Command "Start-Process cmd -ArgumentList '/c %~s0 %*' -Verb RunAs"
-exit /b
-)
+	powershell -Command "Start-Process cmd -ArgumentList '/c %~s0 %*' -Verb RunAs"
+	exit /b
+	)
 :gotPrivileges 
-cls
+	cls
 ECHO Copying plugins from ElMeow to Fifine
 	xcopy "%AppData%\Elgato\StreamDeck\Plugins\" "%AppData%\HotSpot\StreamDock\plugins\" /d /e /c /i /k /o /y /r >nul 2>&1
 ECHO Copying Icon Packs from ElMeow to Fifine
 	xcopy "%AppData%\Elgato\StreamDeck\IconPacks\" "%AppData%\HotSpot\StreamDock\icons\" /d /e /c /i /k /o /y /r >nul 2>&1
 ECHO Clearing the Fifine store plugins cache
-del /S /Q /F "%AppData%\HotSpot\StreamDock\storecache\storecache.json
-taskkill /f /IM "fifine control deck.EXE" 
-::>nul 2>&1
-cd "C:\Program Files (x86)\fifine Control Deck"
-"fifine Control Deck.exe"
-::PAUSE
+	del /S /Q /F "%AppData%\HotSpot\StreamDock\storecache\storecache.json
+ECHO Restarting the Fifine Control Deck Software
+	taskkill /f /IM "fifine control deck.EXE" >nul 2>&1
+	cd "C:\Program Files (x86)\fifine Control Deck"
+	"fifine Control Deck.exe"
