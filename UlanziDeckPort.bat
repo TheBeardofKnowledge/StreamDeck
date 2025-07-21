f5434f4a6a9aaa0584a35fc202a26d038e6a60d6
The Beard of Knowledge <thebeardofknowledge956@gmail.com>
2025-07-21 16:38:51 -0500

    Update UlanziDeckPort.bat

--- UlanziDeckPort.bat
+++ UlanziDeckPort.bat
@@ -16,12 +16,13 @@ ECHO =============================
 :gotPrivileges 
 	cls
 ECHO Copying plugins from ElMeow to Ulanzi
-	xcopy "%AppData%\Elgato\StreamDeck\Plugins\" "%AppData%\Ulanzi\UlanziDeck\plugins\" /d /e /c /i /k /o /y /r >nul 2>&1
+	xcopy "%AppData%\Elgato\StreamDeck\Plugins\" "%AppData%\Ulanzi\UlanziDeck\plugins\" /d /e /c /i /k /o /y /r ::>nul 2>&1
 ECHO Copying Icon Packs from ElMeow to Ulanzi
-	xcopy "%AppData%\Elgato\StreamDeck\IconPacks\" "%AppData%\Ulanzi\UlanziDeck\icons\" /d /e /c /i /k /o /y /r >nul 2>&1
+	xcopy "%AppData%\Elgato\StreamDeck\IconPacks\" "%AppData%\Ulanzi\UlanziDeck\icons\" /d /e /c /i /k /o /y /r ::>nul 2>&1
 ECHO Clearing the Ulanzi store plugins cache and restarting the APP
 	taskkill /f /IM "UlanziDeck.exe" 
  del /S /Q /F "%AppData%\Ulanzi\UlanziDeck\storecache\storecache.json
 		cd "C:\Program Files (x86)\UlanziDeck\"
 	"UlanziDeck.exe"
- 
+If theres any errors above - please copy them and report them in the github 
+PAUSE
